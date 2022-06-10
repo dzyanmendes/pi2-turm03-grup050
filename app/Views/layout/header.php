719e248c4ca91339c4fdc77ff46111ed17b88c5d
jonasfrancobiscari <82737060+jonasfrancobiscari@users.noreply.github.com>
2022-06-10 13:01:19 -0300

    Update header.php
    atualização do titulo dinamico e da estrutura de menus

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Dashboard - SB Admin</title>
+        <title><?php if($titulo) { echo $titulo; } else {echo 'PI2T03G050' ;}         ?></title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="<?php echo base_url("public/"."css/styles.css") ?>" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
@@ -45,21 +45,24 @@
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Cadastros</div>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="<?php echo base_url("public/"."clientes/") ?>">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div> Clientes
                             </a>
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="<?php echo base_url("public/"."contratos/") ?>">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div> Contratos
                             </a>
 
                             <div class="sb-sidenav-menu-heading">Relatórios</div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>Cadastros
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>Clientes
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.html">Clientes</a>
+                                    <a class="nav-link" href="<?php echo base_url("public/"."relatorios/clientes_ativos") ?>">Clientes Ativos</a>
+                                </nav>
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="<?php echo base_url("public/"."relatorios/clientes_inativos") ?>">Clientes Inativos</a>
                                 </nav>
                             </div>
 
@@ -69,9 +72,10 @@
                             </a>
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="layout-static.html">Vencidos</a>
-                                    <a class="nav-link" href="layout-static.html">Vencendo</a>
-                                    <a class="nav-link" href="layout-static.html">A Vencer</a>
+                                    <a class="nav-link" href="<?php echo base_url("public/"."relatorios/contratos_vencidos") ?>">Vencidos</a>
+                                    <a class="nav-link" href="<?php echo base_url("public/"."relatorios/contratos_vencendo") ?>">Vencendo</a>
+                                    <a class="nav-link" href="<?php echo base_url("public/"."relatorios/contratos_avencer") ?>">A Vencer</a>
+                                    <a class="nav-link" href="<?php echo base_url("public/"."relatorios/contratos_todos") ?>">Todos</a>
                                 </nav>
                             </div>
 
